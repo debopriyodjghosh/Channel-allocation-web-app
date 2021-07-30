ff9bff011e2885538029772afaeda072e1fc981d
Debopriyo Ghosh <debopriyodjghosh@gmail.com>
2021-07-30 12:22:24 +0530

    base complete

--- UI/base.php
+++ UI/base.php
@@ -1,98 +1,83 @@
 <?php
-include 'config.php';
-
+include 'config.php'; // DB Connection
+//Base station coordinate
 $base_x = rand(0, 200);
 $base_y = rand(0, 200);
 
-
+//device count and channel count
 if (isset($_POST)) {
     $device_count =  $_POST['device_count'];
     $channel_count = $_POST['channel_count'];
     $priority_device =  $_POST['priority_device'];
-    //delete database
+    //delete previous database
     $sql0 = "DELETE from base";
     $result = $conn->query($sql0);
     $sqli = "DELETE from device";
     $result = $conn->query($sqli);
     $sqlj = "ALTER TABLE device AUTO_INCREMENT = 0;";
     $result = $conn->query($sqlj);
-
+    //insert new data in base
     $sql = "INSERT INTO base (device_count, channel_count, priority_device, base_x, base_y ) 
             VALUES ($device_count, $channel_count, $priority_device, $base_x, $base_y)";
     if ($conn->query($sql) === true) { ?>
-
         <script type="text/javascript">
-            alert('Base Station Placed!');
+            alert('Base Station & Devices Placed!');
             //window.location.href = "device.html";
         </script>
-
     <?php } else {
         echo "<script>alert('Error: ' . $sql . '<br>' . $conn->error')javascript:history.go(-1);</script>";
     }
 }
-
+//insert priority device with high data rate
 for ($i = 0; $i < $priority_device; $i++) { //priority device
     $x_cod = rand(0, 200);
     $y_cod = rand(0, 200);
     $data_rate = rand(21, 30);
-
     $temp = $data_rate / 20;
     $tollerence = 1 / (pow(2, $temp) - 1);
-
     $allocation = 0;
     $x3 = pow(($x_cod - $base_x), 2); //x2-x1 sqr
     $y3 = pow(($y_cod - $base_y), 2); //y2-y1 sqr
     $distance = sqrt($x3 + $y3);
-
     $sql1 = "INSERT INTO device ( data_rate, x_cod, y_cod, distance, tollerence, allocation ) 
     VALUES ($data_rate, $x_cod, $y_cod, $distance, $tollerence, $allocation)";
     if ($conn->query($sql1) === true) { ?>
-
-
     <?php } else {
         echo "<script>alert('Error: ' . $sql1 . '<br>' . $conn->error')javascript:history.go(-1);</script>";
     }
 }
 
-//<script type="text/javascript"> 
-#alert('Device Placed!');
-//window.location.href = "device.html";
-//</script>
-
+//insert non priority device with less data rate
 for ($j = 0; $j < ($device_count - $priority_device); $j++) { //non priority device
     $x_cod = rand(0, 200);
     $y_cod = rand(0, 200);
     $data_rate = rand(1, 20);
-
     $temp = $data_rate / 20;
     $tollerence = 1 / (pow(2, $temp) - 1);
-
     $allocation = 0;
     $x3 = pow(($x_cod - $base_x), 2); //x2-x1 sqr
     $y3 = pow(($y_cod - $base_y), 2); //y2-y1 sqr
     $distance = sqrt($x3 + $y3);
-
     $sql1 = "INSERT INTO device ( data_rate, x_cod, y_cod, distance, tollerence, allocation ) 
     VALUES ($data_rate, $x_cod, $y_cod, $distance, $tollerence, $allocation)";
     if ($conn->query($sql1) === true) { ?>
-
-
+        
 <?php } else {
         echo "<script>alert('Error: ' . $sql1 . '<br>' . $conn->error')javascript:history.go(-1);</script>";
     }
 }
+//display table data 
 ?>
 
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Channel Page</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="style.css" rel="stylesheet">
 </head>
 
-<body>
+<body >
     <div class="login">
         <div class="heading">
             <h2>Base and Device Details</h2>
@@ -104,7 +89,7 @@ for ($j = 0; $j < ($device_count - $priority_device); $j++) { //non priority dev
         <div class="heading">
             <div class="card-body table-responsive p-0" style="height: 100px;">
                 <table class="table table-head-fixed text-nowrap">
-                    <thead>
+                    <thead-dark>
                         <tr>
                             <th>Base ID</th>
                             <th>Device Count</th>
@@ -116,10 +101,9 @@ for ($j = 0; $j < ($device_count - $priority_device); $j++) { //non priority dev
                     </thead>
                     <tbody>
                         <?php
-
+                        //display base detals
                         $sql2 = "SELECT * from base";
                         $result = $conn->query($sql2);
-
                         if (mysqli_num_rows($result) > 0) {
                             while ($row = mysqli_fetch_array($result)) {
                                 echo " <tr>";
@@ -132,20 +116,18 @@ for ($j = 0; $j < ($device_count - $priority_device); $j++) { //non priority dev
                                 echo "</tr>";
                             }
                         } ?>
-
-
                     </tbody>
                 </table>
             </div>
         </div>
         <div class="heading">
             <h3>View Device Details</h3>
+            <hr>
         </div>
         <div class="heading">
-
             <div class="card-body table-responsive p-0" style="height: 600px;">
                 <table class="table table-head-fixed text-nowrap">
-                    <thead>
+                    <thead-dark>
                         <tr>
                             <th>Dev ID</th>
                             <th>Data Rate</th>
@@ -158,16 +140,14 @@ for ($j = 0; $j < ($device_count - $priority_device); $j++) { //non priority dev
                     </thead>
                     <tbody>
                         <?php
-
+                        //display device table
                         $sql2 = "SELECT * from device";
                         $result = $conn->query($sql2);
-
                         if (mysqli_num_rows($result) > 0) {
                             while ($row = mysqli_fetch_array($result)) {
                                 echo " <tr>";
                                 echo "<td>" . $row['dev_id'] . "</td>";
                                 echo "<td>" . $row['data_rate'] . "</td>";
-
                                 echo "<td>" . $row['x_cod'] . "</td>";
                                 echo "<td>" . $row['y_cod'] . "</td>";
                                 echo "<td>" . $row['distance'] . "</td>";
@@ -176,25 +156,19 @@ for ($j = 0; $j < ($device_count - $priority_device); $j++) { //non priority dev
                                 echo "</tr>";
                             }
                         } ?>
-
-
                     </tbody>
                 </table>
             </div>
-
-
-
         </div>
-
+        
+        <button onclick="location.href='device.php'" type ="button" class="btn btn-block btn-danger" value=" ">Start Allocation</button>
+    </td>
     </div>
     </div>
     <hr>
-
-
-
 </body>
-
 </html>
 <?php
-//$conn->close();
+$conn->close();
 ?>
+
--- UI/device.php
+++ UI/device.php
@@ -1,35 +1,19 @@
 <?php
 include 'config.php';
 
-$x_cod=rand(0,200);
-$y_cod=rand(0,200);
-$data_rate=rand(20,30);
 
 
-$temp=$data_rate/20;
-$tollerence=1/(pow(2, $temp)-1);
-
-$allocation=0;
-
-
-$distance=0;// print base co ordinate
-//distance between base and device
-
-
-
-    //echo $priority_device." ".$channel_count." ". $device_count." ".$base_x ." ".$base_y ;
-    $sql = "INSERT INTO device (device_count, channel_count, priority_device, base_x, base_y ) 
-            VALUES ($device_count, $channel_count, $priority_device, $base_x, $base_y)";
+  /*  $sql = "SELECT * from device where data_rate < '20' order by data_rate";
     if ($conn->query($sql)=== true) { ?>
        
         <script type="text/javascript"> 
-                alert('Device Placed!'); 
+                alert('Device Sorted!'); 
                 window.location.href = "#";
                 </script>
         
    <?php } else {
         echo "<script>alert('Error: ' . $sql . '<br>' . $conn->error')javascript:history.go(-1);</script>";
-    }
+    }*/
 
     
 //$conn->close();
